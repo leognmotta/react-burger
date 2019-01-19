30154f8813b1899147db70f625290f8d1aa8f678
Leonardo Motta <leo@domboscoatacado.com.br>
2019-01-19 07:52:38 -0500

    Basic Structure

---
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class BugerBuilder extends Component {
+  render() {
+    return (
+      <>
+        <div>Burguer</div>
+        <div>Build Controls</div>
+      </>
+    );
+  }
+}
+
+export default BugerBuilder;
